239fec571b8471e460583cc1c9481d8cd8d64d6b
Rajpal Singh <ramansmalsar4@gmail.com>
2026-04-17 22:56:12 -0700

    Initial Commit

---
+++ trainmyai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trainmyai
+//
+//  Created by Raman on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trainmyai/trainmyaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainmyaiApp.swift
+//  trainmyai
+//
+//  Created by Raman on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct trainmyaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
